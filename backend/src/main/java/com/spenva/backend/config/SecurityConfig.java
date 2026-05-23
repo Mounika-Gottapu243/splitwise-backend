b1129bbde3b0f58dd95e28b1d5e100a60e5f7078
gottapu mounika <gottapumounika@gottapus-MacBook-Air.local>
2026-05-24 01:40:20 +0530

    changes

--- backend/src/main/java/com/spenva/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/spenva/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173", "https://splitwise-backend-five.vercel.app"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
