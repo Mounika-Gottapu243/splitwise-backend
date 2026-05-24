a4af64e43cab57cd7a34f4268217257faf4345ea
gottapu mounika <gottapumounika@gottapus-MacBook-Air.local>
2026-05-24 09:26:42 +0530

    vercal CORS

--- backend/src/main/java/com/spenva/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/spenva/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173", "https://*.vercel.app"));
+                    config.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://*.vercel.app"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
