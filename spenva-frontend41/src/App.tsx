818bafaa514dfebc14b29a0dfab6aa5d84127760
gottapu mounika <gottapumounika@gottapus-MacBook-Air.local>
2026-05-24 23:02:22 +0530

    frontend and port update

--- spenva-frontend41/src/App.tsx
+++ spenva-frontend41/src/App.tsx
@@ -1,4 +1,5 @@
-﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+﻿
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login";
 import ForgotPasswordPage from "./pages/ForgotPassword";
 import Dashboard from "./pages/Dashboard";
